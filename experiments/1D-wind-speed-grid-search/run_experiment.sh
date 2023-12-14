178305d3397bbdf0d618ff0cb336463ca6c7c5b4
amarcozzi <anthonymarcozzi@gmail.com>
2023-12-14 16:55:37 -0700

    Add example data (will generate later)

--- experiments/1D-wind-speed-grid-search/run_experiment.sh
+++ experiments/1D-wind-speed-grid-search/run_experiment.sh
@@ -1,12 +1,12 @@
 echo "Running experiment: 1D-wind-speed-grid-search"
 
-# Check if the output directory exists
-if [ -d "output" ]; then
-  # Remove the output directory
-  echo "Removing output directory"
-  rm -rf output
-fi
-mkdir output
+# # Check if the output directory exists
+# if [ -d "output" ]; then
+#   # Remove the output directory
+#   echo "Removing output directory"
+#   rm -rf output
+# fi
+# mkdir output
 
 # Submit the job to slurm
 sbatch submit.slurm
