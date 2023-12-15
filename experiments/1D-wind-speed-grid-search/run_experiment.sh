892bf149840bedb454630986126387d3bcb4a7dc
amarcozzi <anthonymarcozzi@gmail.com>
2023-12-14 17:00:00 -0700

    Update directories

--- experiments/1D-wind-speed-grid-search/run_experiment.sh
+++ experiments/1D-wind-speed-grid-search/run_experiment.sh
@@ -1,12 +1,8 @@
 echo "Running experiment: 1D-wind-speed-grid-search"
 
-# # Check if the output directory exists
-# if [ -d "output" ]; then
-#   # Remove the output directory
-#   echo "Removing output directory"
-#   rm -rf output
-# fi
-# mkdir output
+# Check if the output directory exists
+echo "Removing old output files"
+rm out_*
 
 # Submit the job to slurm
 sbatch submit.slurm
