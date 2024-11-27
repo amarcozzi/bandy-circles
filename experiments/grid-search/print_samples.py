e871bd0502e71c3f2f73414e20e9d64e9d5b286e
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2024-11-27 08:50:24 -0700

    Remove print_samples.py script

--- experiments/grid-search/print_samples.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-# Generate 10 wind samples from 1 to 3 m/s
-wind_speeds = np.linspace(1, 3, 10)
-wind_speeds_str = ", ".join([f"{speed:.2f}" for speed in wind_speeds])
-print(f"Wind speeds: [{wind_speeds_str}]")
