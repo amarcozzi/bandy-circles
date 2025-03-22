d151b706ecf74f3a7ce173ad555ca3901a394695
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-03-22 07:53:09 -0600

    Remove environment preparation steps from run_experiment.sh

--- experiments/grid-search-fine/run_experiment.sh
+++ experiments/grid-search-fine/run_experiment.sh
@@ -6,11 +6,6 @@ rm -rf output
 # Create the output directory
 mkdir output
 
-# Prep environment
-source $PROJECT_DIR/miniforge3/etc/profile.d/conda.sh
-conda activate bandy-circles
-module load intel-oneapi-mpi
-
 # Submit the job to slurm
 sbatch submit.slurm
 squeue -u $USER
