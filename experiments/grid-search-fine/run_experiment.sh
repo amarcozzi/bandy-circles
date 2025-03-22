303b884ecc797392a0d7df04854386614b6101ee
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-03-22 07:44:57 -0600

    Prepare environment for experiment execution

--- experiments/grid-search-fine/run_experiment.sh
+++ experiments/grid-search-fine/run_experiment.sh
@@ -6,6 +6,10 @@ rm -rf output
 # Create the output directory
 mkdir output
 
+# Prep environment
+conda activate bandy-circles
+module load intel-oneapi-mpi
+
 # Submit the job to slurm
 sbatch submit.slurm
 squeue -u $USER
