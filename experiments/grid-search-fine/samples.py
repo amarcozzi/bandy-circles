0c7b85718d89cdc3a1f5933297f700ae513fdca5
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-03-22 07:38:05 -0600

    Add grid-search-fine experiment

---
+++ experiments/grid-search-fine/samples.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+wind_speeds = np.linspace(1., 1.9, 10)
+
+print(wind_speeds.round(2).tolist())
+
+circle_radius = 1.8
+
+circle_radius_samples = [0.5 * circle_radius, 0.75 * circle_radius, circle_radius, 1.25 * circle_radius,
+                         1.5 * circle_radius]
+print(circle_radius_samples)
