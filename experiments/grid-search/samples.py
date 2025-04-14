20cffa8bbcd945767774958a02d4fd77902be587
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-04-14 15:27:54 -0600

    Remove old experiments

--- experiments/grid-search/samples.py
+++ experiments/grid-search/samples.py
@@ -6,5 +6,6 @@ print([f"{speed:.2f}" for speed in wind_speeds])
 
 circle_radius = 1.8
 
-circle_radius_samples = [0.5 * circle_radius, 0.75 * circle_radius, circle_radius, 1.25 * circle_radius, 1.5 * circle_radius]
+circle_radius_samples = [0.5 * circle_radius, 0.75 * circle_radius, circle_radius, 1.25 * circle_radius,
+                         1.5 * circle_radius]
 print(circle_radius_samples)
